cde3adc0db93013579c3b86738e80f86f9e5af9d
“silkyh13” <silkyh13@gmail.com>
2020-05-21 21:22:34 -0400

    can upload image to digitalocean

---
+++ server/index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3001;
+const AWS = require('aws-sdk');
+const fs = require('fs'); // Needed for example below
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
+const spacesEndpoint = new AWS.Endpoint('sfo2.digitaloceanspaces.com');
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.SPACES_KEY,
+    secretAccessKey: process.env.SPACES_SECRET
+});
+
+app.get("/api/test", (req, res) => {
+  res.send("Works");
+})
+
+app.post("/api/image", upload.single("image"), (req, res) => {
+  const params = {
+    Bucket: process.env.SPACES_S3_BUCKET,
+    ContentEncoding: "base64",
+    ContentType: "image/*",
+    Key: `images/${req.file.originalname}`,
+    Body: req.file.buffer,
+  };
+
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack)
+    }else {
+      res.send(
+        s3.getSignedUrl("getObject", {
+          Bucket: process.env.SPACES_S3_BUCKET,
+          Key: `images/${req.file.originalname}`,
+        })
+      );
+      console.log('yay it uploaded')
+    }
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
